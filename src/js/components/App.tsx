e66405125386ad9946907c89c010720d3b77172f
0x1eef <0x1eef@protonmail.com>
2024-04-05 15:03:26 -0300

    Add Footer component

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ErrorRenderer } from "~/components/ErrorRenderer";
 import { ResponseRenderer } from "~/components/ResponseRenderer";
+import { Loader } from "~/components/Loader";
 import { useWebService } from "~/hooks/useWebService";
 
 export function App() {
@@ -11,11 +12,6 @@ export function App() {
   } else if (error) {
     return <ErrorRenderer error={error} />;
   } else {
-    return (
-      <div className="w-full h-48" data-testid="loader">
-        <div className="flex flex-col h-5/6 justify-center w-3/4 m-auto"></div>
-        <footer className="flex items-center justify-center h-10">{t("loading")}</footer>
-      </div>
-    );
+    return <Loader />;
   }
 }
--- src/js/components/ErrorRenderer.tsx
+++ src/js/components/ErrorRenderer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Footer } from "~/components/Footer";
 
 export function ErrorRenderer({ error }: { error: Error }) {
   const t = chrome.i18n.getMessage;
   return (
     <div data-testid="error" className="w-full h-48">
       <div className="flex flex-col h-5/6 justify-center w-3/4 m-auto"></div>
-      <footer className="flex items-center justify-center h-10">
+      <Footer>
         <span className="font-bold">{t("error")}</span>: {error.message}
-      </footer>
+      </Footer>
     </div>
   );
 }
