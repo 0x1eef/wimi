47170706e3b7372167b12d2e12e16fbb5aab84f5
0x1eef <0x1eef@protonmail.com>
2024-04-05 15:23:49 -0300

    Add per-environment webpack configs

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const process = require('process');
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+ entry: {
+    index: './src/js/index.tsx',
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    alias: { '~': [path.resolve('src/js/')] },
+    roots: [path.resolve('src/js'), path.resolve('node_modules')],
+    modules: [path.resolve('src/js'), path.resolve('node_modules')],
+    extensions: ['.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/html/", to: "html" },
+        { from: "src/css/", to: "css" },
+        { from: "src/manifest.json", to: "manifest.json" },
+        { from: "src/images", to: "images/" },
+        { from: "src/_locales", to: "_locales/" },
+        { from: "src/fonts", to: "fonts/" }
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,6 @@
-const path = require('path');
-const process = require('process');
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  devtool: "inline-source-map",
-  entry: {
-    index: './src/js/index.tsx',
-  },
-  output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'build')
-  },
-  resolve: {
-    alias: { '~': [path.resolve('src/js/')] },
-    roots: [path.resolve('src/js'), path.resolve('node_modules')],
-    modules: [path.resolve('src/js'), path.resolve('node_modules')],
-    extensions: ['.ts', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "src/html/", to: "html" },
-        { from: "src/css/", to: "css" },
-        { from: "src/manifest.json", to: "manifest.json" },
-        { from: "src/images", to: "images/" },
-        { from: "src/_locales", to: "_locales/" },
-        { from: "src/fonts", to: "fonts/" }
-      ],
-    }),
-    new CleanWebpackPlugin(),
-  ],
-}
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+module.exports = merge(
+  common,
+  {mode: "development", devtool: "inline-source-map"},
+)
