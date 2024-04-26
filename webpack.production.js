46e18945ea1f58691e8316e98984bd0840853ded
0x1eef <0x1eef@protonmail.com>
2024-04-25 21:59:53 -0300

    Switch to esbuild-loader

--- webpack.production.js
+++ webpack.production.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+
 module.exports = merge(
   common,
   {
     mode: "production",
+    devtool: false,
+    optimization: { minimize: true },
     output: {path: path.resolve(__dirname, 'build', 'production')}
   }
 )
